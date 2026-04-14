54abf9556752e3c1bfaf06627cb8d085d40a2932
Litih-7705 <nguyethaquan@gmail.com>
2026-04-14 10:32:01 +0700

    Tìm kiếm + Chi tiết

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LaptopController3;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
@@ -9,6 +10,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-
-
 require __DIR__.'/auth.php';
+
+// Route chi tiết sản phẩm
+Route::get('/laptop/chitiet/{id}', [LaptopController3::class, 'chiTiet']);
+
+// Route tìm kiếm (Sửa thành POST cho khớp form trong layout của bạn)
+Route::post('/timkiem', [LaptopController3::class, 'timKiem']);
